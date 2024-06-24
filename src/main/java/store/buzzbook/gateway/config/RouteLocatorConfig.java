d571d7cb219264ff6d81d82a46444043eb034186
Heldennar <krain7127@gmail.com>
2024-06-24 14:39:00 +0900

    fix: 경로 변경

--- src/main/java/store/buzzbook/gateway/config/RouteLocatorConfig.java
+++ src/main/java/store/buzzbook/gateway/config/RouteLocatorConfig.java
@@ -14,14 +14,13 @@ public class RouteLocatorConfig {
 	public RouteLocator myRoute(RouteLocatorBuilder builder) {
 
 		RouteLocator routeLocator = builder.routes().build();
-
 		return builder.routes()
 			.route("coupon-api",
-				p -> p.path("/api/coupons/**")
-					.uri("http://eureka-server:8091/"))
+				p -> p.path("/api/coupons/**").and()
+					.uri("lb://eureka-server:8091/"))
 			.route("core-api",
-				p -> p.path("/api/acount/**")
-					.uri("http://eureka-server:8090/"))
+				p -> p.path("/api/account/**").and()
+					.uri("lb://core-api:8090/"))
 			.build();
 	}
 }
