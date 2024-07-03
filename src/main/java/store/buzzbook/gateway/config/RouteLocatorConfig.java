cecd74350745ba09abbcdc520d1f3d10185a437d
Heldennar <krain7127@gmail.com>
2024-07-03 17:59:48 +0900

    build : route 추가

--- src/main/java/store/buzzbook/gateway/config/RouteLocatorConfig.java
+++ src/main/java/store/buzzbook/gateway/config/RouteLocatorConfig.java
@@ -18,7 +18,7 @@ public class RouteLocatorConfig {
 				p -> p.path("/api/coupons/**").and()
 					.uri("lb://coupon-api:8091/"))
 			.route("core-api",
-				p -> p.path("/api/account/**","/api/payments/**", "/api/products/**", "/api/orders/**")
+				p -> p.path("/api/account/**","/api/payments/**", "/api/products/**", "/api/orders/**","/api/cart/**")
 					.and().uri("lb://core-api:8090/"))
 			.route("auth-api",
 				p -> p.path("/api/auth/**").and()
